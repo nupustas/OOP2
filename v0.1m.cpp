101b9fd3c6e88b2b21fe1b498616ce7ca416a9b1
nupustas <vejasprastienis@gmail.com>
2025-02-11 15:57:06 +0200

    v0.1release

--- v0.1m.cpp
+++ v0.1m.cpp
@@ -19,11 +19,101 @@ int main()
     Stud grupe[MaxStud];
     int students=0;
     
-    while(students < MaxStud)  
-    {
+    
         Stud laik;
         
+        char a;
+    cout<<"Do you want to input everything manually, input names and generate scores, or generate everything?(m/s/r)";
+    cin>>a;
+    while(a!= 'm' && a!= 's' &&a!= 'r') 
+    {   
+        cout<<"Invalid input. Enter m , s or r"<<endl;
+        cin>>a;
+    }
 
+    if(a=='r')//Visko "generavimas"
+    {
+        cout<<"How many students do you want to generate? ";
+        int n;
+        cin>>n;
+    
+        srand(time(NULL));
+        for(int i=0; i<n; i++)
+        {
+            Stud laik;
+            
+            int gender=rand()%2;
+            if(gender==0){
+                laik.Vardas=FNames[rand()%25];
+                laik.Pavarde=FSurnames[rand()%25];
+            }
+            else{
+                laik.Vardas=MNames[rand()%25];
+                laik.Pavarde=MSurnames[rand()%25];
+            }
+           
+            for(int j=0; j<5; j++)
+            {
+                laik.paz[j]= rand()%10;
+            }
+            laik.egz=rand()%11;
+            laik.pazcount=5;
+            if(rand()%2==0) laik.vm='a';
+            else laik.vm='m';
+            grupe[students++]=laik;
+        }
+    }
+     
+    else if(a=='s') //Vardo ir pavardes ivedimas, pazymiu generavimas
+     {
+        cout<<"Input student names manually, generate scores"<<endl;
+
+        while(true)
+        {
+            Stud laik;
+            cout<<"Input name: ";
+            cin>>laik.Vardas;
+            cout<<"Input surname: ";
+            cin>>laik.Pavarde;
+
+            cout<<"How many homework scores do you want to generate? ";
+            cin>>laik.pazcount;
+            srand(time(NULL));
+            for(int i=0; i<laik.pazcount; i++)
+            {
+                laik.paz[i]= rand()%11;
+                
+            }
+                laik.egz=rand()%11;
+            cout<<"Calculate final score with average or median? (a/m)";
+            cin>>laik.vm;
+            while(laik.vm!= 'a' && laik.vm!= 'm') 
+            {   
+                cout<<"Invalid input. Enter a or m"<<endl;
+                cin>>laik.vm;
+            }
+
+
+            grupe[students++]=laik;
+
+            cout<<"Enter more students? (y/n)";
+            char x;
+            cin>>x;
+            while(x!= 'y' && x!= 'n') 
+            {   
+                cout<<"Invalid input. Enter y or n"<<endl;
+                cin>>x;
+            }
+            if(x=='n') break;
+            
+        }
+     }
+    
+     //visko ivedimas ranka
+    else{
+        cout<<"Input students manually"<<endl;
+        while(true)
+        {
         cout<<"Input name: "<<endl;
         cin>>laik.Vardas;
         cout<<"Input surname: "<<endl;
@@ -51,7 +141,7 @@ int main()
         }
 
         grupe[students++]=laik;
-
+    
         cout<<"Enter more students? (y/n)";
         char x;
         cin>>x;
@@ -61,17 +151,18 @@ int main()
             cin>>x;
         }
         if(x=='n') break;
-        
+      }  
     }
 
     for(int i=0; i<students; i++){
-        int suma=0;
+        double suma=0;
             for(int j=0; j<grupe[i].pazcount; j++)
             {
-            suma+=grupe[i].paz[j];}
+            suma+=grupe[i].paz[j];
+            }
 
-            if(grupe[i].vm=='v'){
-                grupe[i].galutinis= 0.4*(suma/grupe[i].pazcount)+0.6*grupe[i].egz;
+            if(grupe[i].vm=='a'){
+                grupe[i].galutinis= 0.4*(suma/grupe[i].pazcount)+ 0.6*grupe[i].egz;
             }
             else if (grupe[i].pazcount%2==0){
                 grupe[i].galutinis=0.4*(grupe[i].paz[grupe[i].pazcount/2] + grupe[i].paz[grupe[i].pazcount/2-1])/2 +0.6*grupe[i].egz;
@@ -86,8 +177,8 @@ int main()
     
 for(int i=0; i<students; i++)
 {
-    cout<<std::left<<setw(10)<<grupe[i].Vardas<<setw(10)<<grupe[i].Pavarde<<setw(10);
-    if(grupe[i].vm == 'a') cout<<std::fixed<<std::setprecision(2)<<grupe[i].galutinis<<"         -"<<endl;
+    cout<<std::left<<setw(15)<<grupe[i].Vardas<<setw(18)<<grupe[i].Pavarde<<setw(7);
+    if(grupe[i].vm == 'a') cout<<std::fixed<<std::setprecision(2)<<grupe[i].galutinis<<"            -"<<endl;
     else cout<<" -                "<<std::fixed<<std::setprecision(2)<<grupe[i].galutinis<<endl;
 }
 
